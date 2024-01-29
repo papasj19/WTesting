18a3c4ee386f77799185077e9ee35433e169d237
Spencer Johnson <spencerjames.johnson@students.salle.url.edu>
2024-01-29 16:35:57 +0100

    forgot to add other files :(

---
+++ portfolio_vue/src/components/Contact_Grid.vue
@@ -0,0 +1,82 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="contact_grid">
+      <div class="contact__box">
+        <i class='bx bx-home contact__icon'></i>
+        <h3 class="contact__title">Location</h3>
+        <span class="contact__description">1234 Street Name,<br> City Name</span>
+      </div>
+      <div class="contact__box">
+        <i class='bx bx-phone contact__icon'></i>
+        <h3 class="contact__title">Phone</h3>
+        <span class="contact__description">+1(234)-567-8910</span>
+      </div>
+      <div class="contact__box">
+        <i class='bx bx-envelope contact__icon'></i>
+        <h3 class="contact__title">Email</h3>
+        <span class="contact__description">dude@guy.com</span>
+      </div>
+      <div class="contact__box">
+        <i class='bx bx-message-rounded-edit contact__icon'></i>
+        <h3 class="contact__title">Chat</h3>
+        <div class="contact__description">
+          <a href="#" class="contact__social"><i class='bx bxl-facebook'></i></a>
+          <a href="#" class="contact__social"><i class='bx bxl-instagram'></i></a>
+          <a href="#" class="contact__social"><i class='bx bxl-twitter'></i></a>
+        </div>
+      </div>
+    </div>
+</template>
+
+<style scoped>
+
+.contact_grid{
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-content: center;
+  column-gap: 5%;
+  padding-top: 4%;
+  padding-bottom: 2.5rem;
+
+}
+
+
+
+.contact__box{
+  background-color: #FFF;
+  border-radius: .5rem;
+  padding: 1.5rem;
+  text-align: center;
+  box-shadow: 0 4px 6px rgba(174,190,205,.3);
+}
+
+.contact__icon, .contact__title{
+  color: blue;
+}
+
+.contact__icon{
+  font-size: 2rem;
+}
+
+.contact__title{
+  font-size: 1.125rem;
+  margin-bottom: 1rem;
+}
+
+
+
+.contact__social {
+  color: lightskyblue;
+  font-size: 1.25rem;
+  margin: 0 .5rem;
+}
+
+.contact__box:hover{
+  box-shadow: 0 6px 8px rgba(174,190,205,.5);
+}
+
+</style>
---
+++ portfolio_vue/src/components/myfooter.vue
@@ -0,0 +1,50 @@
+<script setup xmlns="http://www.w3.org/1999/html">
+
+</script>
+
+<template>
+  <footer class="footer">
+    <div class="footer__container bd-container">
+      <h1 class="footer__title">SJ</h1>
+      <p class="footer__description-text">Thanks for visiting my personal website!<br><br>Lets get in touch?</p>
+
+      <p>Send me an email: <a href="mailto:">mail@domain.com</a></p>
+      <div class="footer__social">
+        <a href="#" class="home__social-icon"><i class='bx bxl-linkedin'></i></a>
+        <a href="#" class="home__social-icon"><i class='bx bxl-github'></i></a>
+        <a href="#" class="home__social-icon"><i class='bx bxl-dribbble'></i></a>
+      </div>
+
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+
+/*===== FOOTER =====*/
+.footer{
+  background-color: #121928;
+  color: rgb(128, 128, 128);
+  text-align: center;
+}
+
+.footer__container{
+  padding: 1.5rem 0;
+}
+
+.footer__title{
+  font-size: 1.5rem;
+  margin-bottom: .5rem;
+  font-weight: 600;
+}
+
+
+.footer__social{
+  margin-bottom: .5rem;
+  margin-top: .5rem;
+}
+
+
+
+
+</style>
